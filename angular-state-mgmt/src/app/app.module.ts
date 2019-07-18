c0c7ab680dffc741d91be92ee2a66ee002893346
Bayer <JBayer@Wheels.com>
2019-07-18 12:41:15 -0500

    clean up directory

--- angular-state-mgmt/src/app/app.module.ts
+++ angular-state-mgmt/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { Example1Component } from './example1/example1/example1.component';
+import { Example1Component } from './example1/example1.component';
 
 @NgModule({
   declarations: [
